c0eef3f47497da76fce32b7aee6d26efaa704289
Vasco Costa <vascomacosta@gmail.com>
2014-12-19 18:58:39 +0000

    first commit

---
+++ owm.go
@@ -0,0 +1,127 @@
+/*
+Package owm implements a wrapper for the OpenWeatherMap API.
+
+Read more about the OpenWeatherMap API here: http://openweathermap.org/api.
+*/
+package owm
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type weatherLine struct {
+	Main        string
+	Description string
+}
+
+type forecastLine struct {
+	Dt   int
+	Main struct {
+		Temp      float64
+		TempMin   float64 `json:"temp_min"`
+		TempMax   float64 `json:"temp_max"`
+		Pressure  float64
+		SeaLevel  float64 `json:"sea_level"`
+		GrndLevel float64 `json:"grnd_level"`
+		Humidity  int
+	}
+	Weather []weatherLine
+	Wind    struct {
+		Speed float64
+		Deg   float64
+		Gust  float64
+	}
+	Clouds struct {
+		All int
+	}
+	Sys struct {
+		Pod string
+	}
+	DtTxt string `json:"dt_txt"`
+}
+
+type Weather struct {
+	Coord struct {
+		Lat float64
+		Lon float64
+	}
+	Sys struct {
+		Country string
+		Sunrise int
+		Sunset  int
+	}
+	Weather []weatherLine
+	Main    struct {
+		Temp      float64
+		Humidity  int
+		TempMin   float64 `json:"temp_min"`
+		TempMax   float64 `json:"temp_max"`
+		Pressure  float64
+		SeaLevel  float64 `json:"sea_level"`
+		GrndLevel float64 `json:"grnd_level"`
+	}
+	Wind struct {
+		Speed float64
+		Deg   float64
+		Gust  float64
+	}
+	Clouds struct {
+		All int
+	}
+	Dt   int
+	Id   int
+	Name string
+	Cod  int
+}
+
+type Forecast struct {
+	Cod  int
+	City struct {
+		Id    int
+		Name  string
+		Coord struct {
+			Lon float64
+			Lat float64
+		}
+		Country string
+	}
+	Cnt          int
+	ForecastLine []forecastLine
+}
+
+type Client struct {
+	key     string
+	baseURL string
+}
+
+// NewClient returns a new client given a key.
+func NewClient(key string) *Client {
+	return &Client{key, "http://api.openweathermap.org/data/2.5/"}
+}
+
+func (c *Client) data(url string) []byte {
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return data
+}
+
+// WeatherByName decodes the current weather given a city name.
+func (c *Client) WeatherByName(name string, units string) Weather {
+	var w Weather
+	data := c.data(c.baseURL + "weather" + "?q=" + name + "&units=" + units)
+	err := json.Unmarshal(data, &w)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return w
+}
